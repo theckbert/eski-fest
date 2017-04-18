1d18e9d62ab818366435a676e2866101327a365a
Tessah Heckbert <theckbert@gmail.com>
2017-04-18 15:34:26 -0500

    changed style-tile.php to index.php

---
+++ index.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Style Tyle</title>
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+<link rel="stylesheet" type="text/css" href="css/eskiboy.css" />
+</head>
+
+<body>
+<!--<div class="left"></div><div class="right"></div>-->
+<div class="container">
+      <div class="row">
+            <div class="col-lg-12">
+                  <img class="center-block" src="images/eski-fest-logo-lg.png" border="0">
+            </div>
+      </div>
+      
+      <div class="row">
+            <div class="col-lg-12 description">
+                  <h4 class="text-center">Grime Music &amp; Arts Celebration</h4>
+            </div>
+      </div>
+
+      <div class="row">
+            <div class="col-lg-12 description">
+                  <h4 class="text-center">Grime Music &amp; Arts Celebration</h4>
+            </div>
+      </div>
+
+</div>
+
+<div class="container">
+      <div class="row">
+            <div class="col-lg-12">
+                  <img class="center-block" src="images/lineup-sm.png" border="0">
+            </div>
+      </div>
+
+      <div class="row">
+            <div class="col-lg-12">
+	<!--<h1>Eski Fest: Git Desktop</h1>-->
+      <!--<div class="glitch" data-text="Eski Fest">Eski Fest</div> -->
+	
+                	<div class="text-center lineup alphabetical">
+                  <!--<div class="lineup alphabetical">-->
+                        <a href=""><span class="name">A$AP Mob</span></a>
+                        <a href=""><span class="name">Abstrakt Sonance</span></a>
+                        <a href=""><span class="name">AJ Tracey</span></a>
+                        <a href=""><span class="name">Alison Wonderland</span></a>
+                        <a href=""><span class="name">AlunaGeorge</span></a>
+                        <a href=""><span class="name">Annie Mac</span></a>
+                        <a href=""><span class="name">Baauer</span></a>
+                        <a href=""><span class="name">Benga</span></a>
+                        <a href=""><span class="name">Big H</span></a>
+                        <a href=""><span class="name">Big Narstie</span></a>
+                        <a href=""><span class="name">Big Zuu</span></a>
+                        <a href=""><span class="name">Bonkaz</span></a>
+                        <a href=""><span class="name">British Man Dem*</span></a>
+                        <a href=""><span class="name">Bugzy Malone</span></a>
+                        <a href=""><span class="name">Cadet</span></a>
+                        <a href=""><span class="name">Chance The Rapper</span></a>
+                        <a href=""><span class="name">Chase and Status</span></a>
+                        <a href=""><span class="name">Childish Gambino</span></a>
+                        <a href=""><span class="name">Chip</span></a>
+                        <a href=""><span class="name">Clams Casino</span></a>
+                        <a href=""><span class="name">D Double E</span></a>
+                        <a href=""><span class="name">Danny Brown</span></a>
+                        <a href=""><span class="name">Danny Seth</span></a>
+                        <a href=""><span class="name">Dave</span></a>
+                        <a href=""><span class="name">Denzel Curry</span></a>
+                        <a href=""><span class="name">Devlin</span></a>
+                        <a href=""><span class="name">Diplo</span></a>
+                        <a href=""><span class="name">Disclosure</span></a>
+                        <a href=""><span class="name">Dizzee Rascal</span></a>
+                        <a href=""><span class="name">Drake</span></a>
+                        <a href=""><span class="name">Faze Miyake</span></a>
+                        <a href=""><span class="name">Fekky</span></a>
+                        <a href=""><span class="name">Footsie</span></a>
+                        <a href=""><span class="name">FreezaChin</span></a>
+                        <a href=""><span class="name">Frisco</span></a>
+                        <a href=""><span class="name">Future</span></a>
+                        <a href=""><span class="name">G Eazy</span></a>
+                        <a href=""><span class="name">Ghetts</span></a>
+                        <a href=""><span class="name">Giggs</span></a>
+                        <a href=""><span class="name">Hermitude</span></a>
+                        <a href=""><span class="name">Hudson Mohawke</span></a>
+                        <a href=""><span class="name">Jack Dat</span></a>     
+                        <a href=""><span class="name">Jamie XX</span></a>
+                        <a href=""><span class="name">Jammer</span></a>     
+                        <a href=""><span class="name">Jammz</span></a>
+                        <a href=""><span class="name">Jayemkayem</span></a>
+                        <a href=""><span class="name">JayKae</span></a>
+                        <a href=""><span class="name">J Hus</span></a>
+                        <a href=""><span class="name">JME</span></a>
+                        <a href=""><span class="name">Joker</span></a>
+                        <a href=""><span class="name">Jook</span></a>
+                        <a href=""><span class="name">Julie Adenuga</span></a>
+                        <a href=""><span class="name">Just Blaze</span></a>
+                        <a href=""><span class="name">Kamakaze</span></a>
+                        <a href=""><span class="name">Kano</span></a>
+                        <a href=""><span class="name">Kanye West</span></a>
+                        <a href=""><span class="name">Kendrick Lamar</span></a>
+                        <a href=""><span class="name">Krept and Konan</span></a>
+                        <a href=""><span class="name">Lady Leshurr</span></a>
+                        <a href=""><span class="name">Lady Sovereign</span></a>
+                        <a href=""><span class="name">Lethal Bizzle</span></a>
+                        <a href=""><span class="name">Logan Sama</span></a>
+                        <a href=""><span class="name">Logic</span></a>
+                        <a href=""><span class="name">Madam X</span></a>
+                        <a href=""><span class="name">MIA</span></a>
+                        <a href=""><span class="name">MistaJam</span></a>
+                        <a href=""><span class="name">Mr Carmack</span></a>
+                        <a href=""><span class="name">Ms Banks</span></a>
+                        <a href=""><span class="name">Nines*</span></a>
+                        <a href=""><span class="name">Novelist</span></a>
+                        <a href=""><span class="name">P Money</span></a>
+                        <a href=""><span class="name">Pete Tong</span></a>
+                        <a href=""><span class="name">Pharrell Williams</span></a>
+                        <a href=""><span class="name">Plastician</span></a>
+                        <a href=""><span class="name">Polonis*</span></a>
+                        <a href=""><span class="name">Preditah</span></a>
+                        <a href=""><span class="name">President T</span></a>
+                        <a href=""><span class="name">Pusha T</span></a>
+                        <a href=""><span class="name">Rae Sremmurd</span></a>
+                        <a href=""><span class="name">RL Grime</span></a>
+                        <a href=""><span class="name">Rocks FOE</span></a>
+                        <a href=""><span class="name">Rude Kid</span></a>
+                        <a href=""><span class="name">Rudimental</span></a>
+                        <a href=""><span class="name">Ruff Sqwad*</span></a>
+                        <a href=""><span class="name">Run the Jewels</span></a>
+                        <a href=""><span class="name">Schoolboy Q</span></a>
+                        <a href=""><span class="name">Section Boyz</span></a>
+                        <a href=""><span class="name">Sir Spyro</span></a>
+                        <a href=""><span class="name">Skepta</span></a>
+                        <a href=""><span class="name">Skream</span></a>
+                        <a href=""><span class="name">Slimzee</span></a>
+                        <a href=""><span class="name">Stormzy</span></a>
+                        <a href=""><span class="name">Tempa</span></a>
+                        <a href=""><span class="name">Tempa T</span></a>
+                        <a href=""><span class="name">Tim Westwood</span></a>
+                        <a href=""><span class="name">Tre Mission</span></a>
+                        <a href=""><span class="name">Trends</span></a>
+                        <a href=""><span class="name">Wale</span></a>
+                        <a href=""><span class="name">Wiley</span></a>
+                        <a href=""><span class="name">Wretch 32</span></a>
+                        <a href=""><span class="name">Young Thug</span></a>
+                        <a href=""><span class="name">Zeds Dead</span></a>
+            	</div>
+            </div>
+      </div>
+</div>
+
+<div class="container">
+      <div class="row">
+            <div class="col-lg-12 footer">
+                  <p class="text-center">&copy; Copyright <?php echo the_date('Y'); ?> <a href="http://whattheheckbert.com/" target="_blank">Tessah Heckbert</a> |  <a href="#">About this Project</a>
+                  </p>
+            </div>
+      </div>
+</div>
+
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
